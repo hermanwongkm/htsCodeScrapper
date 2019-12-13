4984beb42ca5fa9b6b7fef0580d1f88e6377d8e8
Soong Cun Yuan <zengersoong@gmail.com>
2019-12-13 17:03:05 +0800

    Initial upload

---
+++ Backend/focus.py
@@ -0,0 +1,56 @@
+import sys
+import csv
+import numpy
+import nltk
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+import pandas as pd
+from pathlib import Path
+
+nltk.download("stopwords",quiet=True)
+nltk.download('punkt', quiet=True)
+
+filepath = sys.argv[1] # for use with nodeJS
+# filepath= "./database/csv/mined/file.csv" 
+stop_words = nltk.corpus.stopwords.words('english')
+custom_stop_words = ['<' , '>' , ',' , ':' , '(' , ')' ,'[',']', ';','/i']
+stop_words.extend(custom_stop_words)
+
+def loadTable(filepath):
+    with open(filepath, newline='') as csvfile:
+        table = list(csv.reader(csvfile))
+    table = [x + [""] for x in table] # expand by one column
+    table[0][-1] = "Keywords" #add keyword header
+    # table[0][0] = "HTS Number" #fix corrupted column field
+    return table
+
+def modify(table):
+    parent = ""
+    for i in range(1,len(table)):
+        # 1.For isolating focus keywords
+        word_tokens = word_tokenize(table[i][2]) 
+        table[i][-1] = [w for w in word_tokens if not w in stop_words]
+        # 2.For filling in the parent hts code for empty children
+        if(table[i][0]==""):
+            table[i][0] = parent
+        else: parent = table[i][0]
+    return table
+
+# parents=True will also create any necessary parent directories, 
+# and exist_ok=True won't raise an error if the directory already exists, don't have to explicitly check that separately.
+
+def dfToCSV(table):
+    df = pd.DataFrame(table)
+    output_file = 'modified.csv'
+    output_dir = Path('database/csv/modified')
+    output_dir.mkdir(parents=True, exist_ok=True)
+    df.to_csv(output_dir / output_file)
+    return "[2] Sucessfully Modified the mined csv."
+
+
+
+table = loadTable(filepath)
+modifiedTable = modify(table)
+print(dfToCSV(modifiedTable))
+
+sys.stdout.flush()
---
+++ Backend/server.js
@@ -0,0 +1,99 @@
+// Nightmare for data mining automation
+const Nightmare = require("nightmare");
+require("nightmare-download-manager")(Nightmare);
+const nightmare = Nightmare({ show: true });
+
+// Spawn is for python script for data preprocessing
+const spawn = require("child_process").spawn;
+const csvtojson = require("csvtojson");
+
+// MongoDB requirements and variables.
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+var serverAdr = 'localhost:27017',dbName = 'hts'
+mongoose.connect(`mongodb://${serverAdr}/${dbName}`, { useNewUrlParser: true })
+mongoose.connection.on('error', function (err) {
+    if (err) throw err;
+});
+mongoose.set('useCreateIndex', true);
+
+const htsSchema = new mongoose.Schema({
+  0: String,
+  1:String,
+  2: String,
+  3:String,
+  4: String,
+  5:String,
+  6: String,
+  7:String,
+  8: String,
+  9:String,
+});
+// Schema object
+const Record = mongoose.model('hts_codes', htsSchema);
+
+var example = {
+  '0': '0105.94.00.00',
+  '1': '2',
+  '2': 'Raven',
+  '3': '["No.","kg"]',
+  '4': '2¢/kg',
+  '5': 'Free (A+,AU,BH, CA,CL,CO,D,E,IL,JO,KR,MA,MX, OM,P,PA,PE,SG)',
+  '6': '17.6¢/kg',
+  '7': '',
+  '8': '',
+  '9': "['Raven']",
+}
+let record = new Record(example);
+
+/**
+ * getUpdated calls for web automation tool to download the latest csv file.
+ */
+const filepath= "./database/csv/mined/file.csv"
+
+convertCSV = async() => {
+  data = await csvtojson().fromFile("./database/csv/modified/modified.csv")
+  return data;
+};
+
+fetch = async() =>{
+    nightmare.on("download", function(state, downloadItem) {
+        if (state == "started") {
+          nightmare.emit("download", filepath, downloadItem);
+        }
+      });
+      
+      nightmare
+        .downloadManager()
+        .goto("https://hts.usitc.gov/export")
+        .wait(1000)
+        .type("body", "\u000d") // press enter
+        .type(`input[name="from"]`, "0000")
+        .type(`input[name="to"]`, "9999")
+        .click("input#Submit.btn.btn-primary")
+        .wait(10000)
+        .waitDownloadsComplete()
+        .then(() => {
+             // now run python script
+             console.log("[1] Successfully mined hts.usitc.gov/export")
+             const pythonProcess = spawn('python3',["./focus.py", filepath]);
+            pythonProcess.stdout.on('data', async(data) => {
+            console.log(String(data));
+            converted = await convertCSV();
+            Record.collection.insertMany(converted, function (err, docs) {
+              if (err){ 
+                  return console.error(err);
+              } else {
+                console.log(docs.insertedCount+" documents inserted to Collection");
+              }
+            });
+            console.log("[3] Successfully convert .csv to JSON");
+            });
+           });
+    return true
+  }
+// record.save();
+console.log("[0] Nightmare is running... \n [0.1] Mining hts.usitc.gov ...")
+fetch()
+
+
