40f13f648f7d8425783206e7cc8aca3b31df6a3c
Soong Cun Yuan <zengersoong@gmail.com>
2019-12-16 16:13:06 +0800

    Separated Schemas

--- Backend/server.js
+++ Backend/server.js
@@ -10,46 +10,18 @@ const csvtojson = require("csvtojson");
 
 // MongoDB requirements and variables.
 var mongoose = require("mongoose");
+var models = require('./schemas/record.js'); //import records.js
+
+
 mongoose.Promise = global.Promise;
+
+
 var serverAdr = 'localhost:27017',dbName = 'hts'
 mongoose.connect(`mongodb://${serverAdr}/${dbName}`, { useNewUrlParser: true })
 mongoose.connection.on('error', function (err) {
     if (err) throw err;
 });
 mongoose.set('useCreateIndex', true);
-
-const htsSchema = new mongoose.Schema({
-  0: String,
-  1:String,
-  2: String,
-  3:String,
-  4: String,
-  5:String,
-  6: String,
-  7:String,
-  8: String,
-  9:[{
-    type: String
-  }]
-});
-
-var example = {
-  '0': '0105.94.00.00',
-  '1': '2',
-  '2': 'Raven',
-  '3': '["No.","kg"]',
-  '4': '2¢/kg',
-  '5': 'Free (A+,AU,BH, CA,CL,CO,D,E,IL,JO,KR,MA,MX, OM,P,PA,PE,SG)',
-  '6': '17.6¢/kg',
-  '7': '',
-  '8': '',
-  '9': "['Raven']",
-}
-
-// Schema object
-const Record = mongoose.model('hts_codes', htsSchema);
-let record = new Record(example);
-
 
 /**
  * getUpdated calls for web automation tool to download the latest csv file.
@@ -90,7 +62,7 @@ const fetch = async() =>{
             pythonProcess.stdout.on('data', async(data) => {
             console.log(String(data));
             converted = await parseJSON();
-            Record.collection.insertMany(converted, {safe:true}, function (err, docs) {
+            models.Record.collection.insertMany(converted, {safe:true}, function (err, docs) {
               if (err){ 
                   return console.error(err);
               } else {
@@ -104,20 +76,7 @@ const fetch = async() =>{
   }
 
 module.exports.fetch = fetch;
-/*
-Usage
-const server = require('./server');
-console.log(`User: ${server.fetch()}`);
-*/ 
 
 
 fetch()
-
-
-// const query = Record.find(); // 'query is an instance of 'query'
-// query.setOptions({
-//   lean:true, //lean is great for high performance, read only cases.
-// }); // 
-
-// query.findOne({'des'})
 
